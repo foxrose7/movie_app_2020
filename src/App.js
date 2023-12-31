e8a574a5bb55c136218006185434dc92a34a65fe
Lee kihyuck <foxrose@kead.or.kr>
2023-12-31 23:35:06 +0900

    using props test.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from 'react';
+// import Potato from './Potato'
+
+function Food ({fav}){
+  // console.log(props);
+  // const {fav, something} = props;
+  return (
+    <div>
+      <h3>I like {fav}</h3>
+    </div>
+  );
+  // return <h3>I like {something}</h3>;
+}
 function App() {
-  return <div className="App"/>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav='kimch'/>
+      <Food fav='japchae'/>
+      <Food fav='boolgogi'/>
+      <Food fav='bap'/>
+    </div>
+  );
 }
 
 export default App;
