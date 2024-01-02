977946f67b0edfd823c80fef6aeaab296859ef94
Lee kihyuck <foxrose@kead.or.kr>
2024-01-03 00:20:14 +0900

    영화 포스터 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -16,7 +17,7 @@ class App extends React.Component {
         data: { movies },
       },
     } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
     // this.setState({ movies: movies });
     // this.setState({ movies });
@@ -32,8 +33,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? 'Loading...'
+          : movies.map((movie) => {
+              console.log(movie);
+              return<Movie 
+                key={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />;
+            })}
+      </div>
+    );
   }
 }
 
