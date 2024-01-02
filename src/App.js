a67eab49260b3f39f3bc618f49a1bdd5d3e7bb8c
Rhie kihyuck <foxrose@kead.or.kr>
2024-01-02 21:15:32 +0900

    영화 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,67 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import axios from "axios";
 
-const foodILike = [
-  {
-    id: 1,
-    name: '김치',
-    image:
-      'https://i.namu.wiki/i/3u7KQ8jVXWJMJpioMNBo5bFb7NrMd1jUbJrGXy99vSgh57D2w0BHG3RlEnawqyRlfAuTihp37sPnlCKX3IlKNg.webp',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: '삼겹살',
-    image:
-      'https://i.namu.wiki/i/SdI6mHzKiWg-yJEqBpWhaACTth0ZSU_qSjhHCnQp5OxqwVl-PRIzql6bkmp1tm0YaHxjDoTbt-nBtiSE4nhqbQ.webp',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: '비빔밥',
-    image:
-      'https://i.namu.wiki/i/dgjXU86ae29hDSCza-L0GZlFt3T9lRx1Ug9cKtqWSzMzs7Cd0CN2SzyLFEJcHVFviKcxAlIwxcllT9s2sck0RA.jpg',
-    rating: 4.3,
-  },
-];
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    // https://github.com/serranoarevalo/yts-proxy
+
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // this.setState({ movies: movies });
+    // this.setState({ movies });
+    // this.setState({ movies: movies, isLoading: false });
+    this.setState({ movies, isLoading: false });
+
+    console.log(movies);
+    // console.log(movies.data.data.movies);
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
-
-// function renderFood(dish){
-//   return <Food name={dish.name} picture={dish.image} />;
-// }
-
-// const renderFood = (dish) => {
-//   return <Food name={dish.name} picture={dish.image} rating={dish.rating} />;
-// };
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {/* {foodILike.map(renderFood)} */}
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
